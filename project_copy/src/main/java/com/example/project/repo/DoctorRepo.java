853928295318f991db9468144c127990d87974c4
lixxie29 <lixxxieee@gmail.com>
2024-09-25 16:36:59 +0300

    initial commit ig

--- project_copy/src/main/java/com/example/project/repo/DoctorRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.repo;
-
-import com.example.project.entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepo extends JpaRepository<Doctor,Long> {
-    Doctor findByEmail(String email);
-}
--- project_copy/src/main/java/com/example/project/service/DoctorSpecialtyService.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.service;
-
-import com.example.project.entity.DoctorSpecialty;
-
-public interface DoctorSpecialtyService {
-    DoctorSpecialty findSpecialtyById(Long id);
-}
