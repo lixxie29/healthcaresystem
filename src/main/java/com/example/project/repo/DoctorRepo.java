f251fe9a3f93e297635fdb33f3c4b7ae109d5e27
lixxie29 <lixxxieee@gmail.com>
2024-09-30 13:56:28 +0300

    1:56pm

--- src/main/java/com/example/project/repo/DoctorRepo.java
+++ src/main/java/com/example/project/repo/DoctorRepo.java
@@ -1,8 +1,11 @@
 package com.example.project.repo;
 
 import com.example.project.entity.Doctor;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface DoctorRepo extends JpaRepository<Doctor,Long> {
